f87ecc220c827bfb56b7773a8d58439ac5dab099
Javier <framirez76@uabc.edu.mx>
2021-10-09 14:26:29 -0700

    Type conversion in python

---
+++ day 2/type_conversion.py
@@ -0,0 +1,10 @@
+num_char = len(input("What is your name?"))
+
+#convert number to string
+print("Your name is " + str(num_char) + " characters long.")
+
+#convert integer to float
+a = float(4235)
+
+#Convert number to string
+print(str(100) + str(70))
