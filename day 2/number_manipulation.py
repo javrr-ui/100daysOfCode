c7adcadbeb0e506506a5e07dff9c112d9d135808
Javier <framirez76@uabc.edu.mx>
2021-10-10 13:30:56 -0700

    Number manipulation and F String in python

---
+++ day 2/number_manipulation.py
@@ -0,0 +1,18 @@
+print(round(8/3,2))
+
+#Floor
+
+print(8 // 3)
+
+# result /= 2
+# result *= 2
+# result += 2
+# result -= 2
+
+#F Strings
+
+score = 0
+height = 1.7
+isWinning = True
+
+print(f"Your score is {score}, your height is: {height}, you are winning is {isWinning}")
