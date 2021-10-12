9b11a32804f5e035fdc5b80a892744be9fbe9701
Javier <framirez76@uabc.edu.mx>
2021-10-11 18:13:41 -0700

    Nested if statements and elif

--- day 3/conditional_operators.py
+++ day 3/conditional_operators.py
@@ -3,5 +3,13 @@ height = int(input("What is your height in cm? "))
 
 if height > 120:
     print("You can ride the rollercoaster")
+    age = int(input("Please enter your age: "))
+
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $11.")
 else:
     print("You can't ride the rollercoaster")
