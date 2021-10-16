d01112acf0d82689bb596da73ddf528af79a0165
Javier <framirez76@uabc.edu.mx>
2021-10-16 14:54:36 -0700

    Range function

--- day 5/for_loop.py
+++ day 5/for_loop.py
@@ -3,3 +3,9 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
+
+#Range function
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
