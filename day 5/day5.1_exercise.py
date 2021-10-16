b70c651a7f0d677922c5c638d2e68f9af3311d92
Javier <framirez76@uabc.edu.mx>
2021-10-16 13:44:05 -0700

    Day 5 exercise 1: Average height

---
+++ day 5/day5.1_exercise.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+counter = 0 
+for height in student_heights:
+    counter += 1
+    sum += height
+
+average = round(sum / counter)
+print(average)
