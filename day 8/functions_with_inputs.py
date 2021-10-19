7d64d8e9ba46cc4054fa29312d4688d5c369986d
Javier <framirez76@uabc.edu.mx>
2021-10-19 11:04:26 -0700

    Functions with inputs

---
+++ day 8/functions_with_inputs.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    print("Hello")
+    print("Hello")
+    print("Hello")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do yo do {name}?")
+
+greet_with_name("Javier")
