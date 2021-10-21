a1df0b4d84773efb8f12e55b177ebadbef83c2a8
Javier <framirez76@uabc.edu.mx>
2021-10-20 20:34:55 -0700

    Caesar cipher part 1: encryption

---
+++ day 8/day8_project.py
@@ -0,0 +1,52 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
+   
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+    length = len(alphabet) - 1
+    cipher_text = ""
+    # Iterate throught each letter from word stored in "text"
+    for letter in text:
+        #Iterate throught alphabet
+        for let in alphabet:
+            if letter == let:
+                shifted_index = 0
+                #if shifted_index greater than length, reduce it until it is a valid index
+                if alphabet.index(let) + shift > length:
+                    shifted_index = (alphabet.index(let) + shift) - length - 1
+                    while True:
+                        if shifted_index > length:
+                            shifted_index -= 26
+                        else:
+                            break   
+                else:
+                    # Add letter from alphabet shifted n numbers
+                    shifted_index = alphabet.index(let) + shift
+                
+                cipher_text += alphabet[shifted_index]
+
+    print(f"The encoded text is {cipher_text}")
+
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    pass
+else:
+    print("U wrong, that option does not exist") 
