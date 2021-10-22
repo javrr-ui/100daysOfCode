b59b4b2a04a4621334b39faf1d2379c4767afea3
Javier <framirez76@uabc.edu.mx>
2021-10-21 19:17:33 -0700

    Refactor code, add loop asking user if wants to encode/decode again

--- day 8/day8_project.py
+++ day 8/day8_project.py
@@ -1,27 +1,31 @@
 from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def caesar(text, shift, direction):
     new_text = ""
+    
 
     for letter in text:
         if letter not in alphabet:
             new_text += letter
         else:
-            position = shift % len(alphabet)
             if direction == "encode":
-                position = alphabet.index(letter) + position
+                position = (alphabet.index(letter) + shift) % len(alphabet)
+                new_text += alphabet[position]
             elif direction == "decode":
-                position = alphabet.index(letter) - position
+                position = (alphabet.index(letter) - shift) % len(alphabet)
+                new_text += alphabet[position]
             else:
                 print("That option does not exist")
                 break
-            new_text += alphabet[position]
+            
     
     print(f"The {direction}d text is {new_text}")
-    
-caesar(text, shift, direction)
+option = "yes"
+while option == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text, shift, direction)
+    option = input("Type 'yes' if you want to go again. Otherwise type 'no'.")
