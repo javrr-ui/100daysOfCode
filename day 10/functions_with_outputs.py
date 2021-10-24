3c66a59248d9a795c0931f17ae7c48e2d9768d75
Javier <framirez76@uabc.edu.mx>
2021-10-24 15:33:20 -0700

    Multiple return values

--- day 10/functions_with_outputs.py
+++ day 10/functions_with_outputs.py
@@ -1,6 +1,13 @@
 #Functions with outputs
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "Invalid input"
+
     return f"{f_name} {l_name}".title()
 
-print(format_name("francisco", "javier"))
+f_name = input("What's your first name? ")
+l_name = input("What's yout last name? ")
+
+
+print(format_name(f_name, l_name))
