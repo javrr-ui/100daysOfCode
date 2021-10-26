d2b5704f1c3f1e1082460c7601eeaf339b3ef3ae
Javier <framirez76@uabc.edu.mx>
2021-10-26 16:48:22 -0700

    Define clear function, call clear function before play() function

--- day 11/day11_project.py
+++ day 11/day11_project.py
@@ -30,12 +30,13 @@ from art import logo
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def clear():
+    _ = system("cls")
 
 def play():
     if not input('Do you want to play a game of Blackjack? Type "y" or "n": ').lower() == "y":
+        clear()
         play()
 
-    
-
-
+clear()
 play()
