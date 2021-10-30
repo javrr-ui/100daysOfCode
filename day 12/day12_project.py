8da91117d89481dabc01de23697f729aacf7cf3e
Javier <framirez76@uabc.edu.mx>
2021-10-30 15:32:18 -0700

    Define play_game, show logo, ask difficulty

--- day 12/day12_project.py
+++ day 12/day12_project.py
@@ -4,3 +4,15 @@ from os import system
 
 def clear():
     _ = system("cls")
+
+def play_game():
+    #clear console before starting the game
+    clear()
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    difficulty = input('Choose a difficulty. Type "easy" or "hard": ')
+    if not difficulty == "easy" or not difficulty == "hard":
+        input("That's not a valid option, please try again\nPress Enter to continue")
+        play_game()
+
+play_game()
