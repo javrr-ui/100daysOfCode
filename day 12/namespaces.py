44a6769aa835b54ec950cc2c28d7da20a4343ae8
Javier <framirez76@uabc.edu.mx>
2021-10-30 14:35:07 -0700

    How to modify a global variable

--- day 12/namespaces.py
+++ day 12/namespaces.py
@@ -15,3 +15,21 @@ def drink_potion():
     print(player_health)
 
 drink_potion()
+
+
+#How to modify a global variable
+
+enemies = 1
+
+print(f"enemy count: {enemies}")
+
+def increase_enemy():
+    #Avooid modifying variables using the global keyword 
+    # global enemies
+    # enemies += 1
+    return enemies + 1
+
+enemies = increase_enemy()
+print(f"enemy count: {enemies}")
+
+
