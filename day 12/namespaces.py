8056ef8797f88768d79ab36b00165c87773b2877
Javier <framirez76@uabc.edu.mx>
2021-10-30 13:27:33 -0700

    Global scope

--- day 12/namespaces.py
+++ day 12/namespaces.py
@@ -1,7 +1,17 @@
 #Local scope
 
+# def drink_potion():
+#     potion_strenght = 2
+#     print(potion_strenght)
+
+# drink_potion()
+
+#Global scope
+
+player_health = 10
+
 def drink_potion():
     potion_strenght = 2
-    print(potion_strenght)
+    print(player_health)
 
 drink_potion()
