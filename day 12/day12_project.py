fb0550ea62a3dcd8b0dffabf2f3f29a6dee0fc5d
Javier <framirez76@uabc.edu.mx>
2021-10-31 01:48:18 -0700

    Improve ui

--- day 12/day12_project.py
+++ day 12/day12_project.py
@@ -5,6 +5,10 @@ from os import system
 def clear():
     _ = system("cls")
 
+def print_ui():
+    clear()
+    print(logo)
+    print("I'm thinking of a number between 1 and 100")
 def play_game():
     random_number = random.randint(0,100)
     
@@ -22,27 +26,28 @@ def play_game():
             attempts = 10
         if difficulty == "hard":
             attempts = 5
-
-        print("I'm thinking of a number between 1 and 100")
+        
+        print_ui()
         while attempts > 0:
+            
             print(f"You have {attempts} attempts left to guess the number.")
             user_answer = int(input("Make a guess: "))
+            
             if user_answer == random_number:
+                print_ui()
                 print(f"You got it! The answer was {random_number}")
                 break
             elif user_answer < random_number:
-                print("Too low.")
+                print_ui()
+                print(f"{user_answer} is too low.")
                 if attempts == 1:
                     print("You lose")
-                else:
-                    print("Guess again.")
                 attempts -= 1
             elif user_answer > random_number:
-                print("Too high.")
+                print_ui()
+                print(f"{user_answer} is too high.")
                 if attempts == 1:
                     print("You lose")
-                else:
-                    print("Guess again.")
                 attempts -= 1
 
 play_game()
