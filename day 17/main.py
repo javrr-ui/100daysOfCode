56cba155e6020bc48b02289f5fb3ca0baa1df669
Javier <framirez76@uabc.edu.mx>
2021-11-07 15:22:50 -0800

    Class constructors and the __init__() function

--- day 17/main.py
+++ day 17/main.py
@@ -1,5 +1,10 @@
 class User:
-    pass
+
+    def __init__(self, user_id, username):
+        print("user created")
+        self.id = user_id
+        self.username = username
 
 
-user1 = User()
+user1 = User("001", "Javier")
+print(user1.id, user1.username)
