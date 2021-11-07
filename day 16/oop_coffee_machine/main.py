325e2d26b21ec33e4a01562c2da5584504bbec3c
Javier <framirez76@uabc.edu.mx>
2021-11-07 12:46:01 -0800

    Ask for user input

--- day 16/oop_coffee_machine/main.py
+++ day 16/oop_coffee_machine/main.py
@@ -1,3 +1,13 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+
+
+def run():
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    run()
+
+
+run()
