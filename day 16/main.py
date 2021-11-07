9ee710bd5835204928338fbf54c2d8cd3f01de71
Javier <framirez76@uabc.edu.mx>
2021-11-07 12:02:47 -0800

    Practice modifying object attributes and calling methods

--- day 16/main.py
+++ day 16/main.py
@@ -7,4 +7,13 @@
 # my_screen = Screen()
 # my_screen.exitonclick()
 
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.field_names = ["Pokemon name", "Type"]
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Squirtle", "Water"])
+table.add_row(["Charmander", "Fire"])
+table.align = "l"
+print(table)
