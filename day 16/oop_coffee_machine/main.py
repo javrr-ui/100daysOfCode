aea22e2603cd0c8701d057e3c29a72049a5fa843
Javier <framirez76@uabc.edu.mx>
2021-11-07 13:05:04 -0800

    Add if else structure, when user enters "report" or a certain drink

--- day 16/oop_coffee_machine/main.py
+++ day 16/oop_coffee_machine/main.py
@@ -9,7 +9,15 @@ def run():
     choice = input(f"What would you like? ({menu.get_items()}): ")
     if choice == "off":
         print("Coffee machine is now turned Off")
-    run()
+    elif choice == "report":
+        # Print report
+        run()
+    else:
+        drink = menu.find_drink(choice)
+        if drink:
+            pass
+        run()
+
 
 
 run()
