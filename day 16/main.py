e125ab141677281f556ffbfcb369c43cbfca5ab1
Javier <framirez76@uabc.edu.mx>
2021-11-07 11:47:57 -0800

    How to add python packages and use Pypi

--- day 16/main.py
+++ day 16/main.py
@@ -1,8 +1,10 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("black", "green")
+# timmy.forward(100)
+# my_screen = Screen()
+# my_screen.exitonclick()
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("black", "green")
-timmy.forward(100)
-my_screen = Screen()
-my_screen.exitonclick()
+import prettytable
