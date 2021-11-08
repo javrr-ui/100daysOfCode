bcdb4833d3f7ca1aac93b6c9039409c0d69add7d
Javier <framirez76@uabc.edu.mx>
2021-11-07 23:41:28 -0800

    Define follow method

--- day 17/main.py
+++ day 17/main.py
@@ -7,6 +7,10 @@ class User:
         self.followers = 0
         self.following = 0
 
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 user1 = User("001", "Javier")
 print(user1.id, user1.username)
