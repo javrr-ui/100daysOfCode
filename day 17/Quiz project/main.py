02b12e5174e4d2088e9ec116174fc021387ff3e9
Javier <framirez76@uabc.edu.mx>
2021-11-08 12:50:11 -0800

    Create question objects from question data

--- day 17/Quiz project/main.py
+++ day 17/Quiz project/main.py
@@ -1,3 +1,11 @@
 from question_model import Question
 from data import question_data
 
+question_bank = []
+
+for question in question_data:
+    text = question['text']
+    answer = question['answer']
+
+    question_obj = Question(text, answer)
+    question_bank.append(question_obj)
