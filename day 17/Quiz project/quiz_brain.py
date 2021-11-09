f09460e2c6d3c47cadac86f48b6df9b4fdb41e11
Javier <framirez76@uabc.edu.mx>
2021-11-08 18:35:58 -0800

    Print current score

--- day 17/Quiz project/quiz_brain.py
+++ day 17/Quiz project/quiz_brain.py
@@ -21,3 +21,5 @@ class QuizBrain:
         else:
             print("You're wrong!")
         print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
