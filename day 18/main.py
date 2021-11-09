a8136d4907b5e120675426dcbd1d65e031e697f2
Javier <framirez76@uabc.edu.mx>
2021-11-08 23:02:15 -0800

    Draw a dashed line

--- day 18/main.py
+++ day 18/main.py
@@ -7,9 +7,18 @@ def draw_square(turtle_obj):
         turtle_obj.right(90)
 
 
+def draw_dashed_line(turtle_obj):
+    for _ in range(15):
+        turtle_obj.forward(10)
+        turtle_obj.penup()
+        turtle_obj.forward(10)
+        turtle_obj.pendown()
+
+
 turtle = Turtle()
 turtle.shape("turtle")
 turtle.color("black", "green")
 draw_square(turtle)
+draw_dashed_line(turtle)
 screen = Screen()
 screen.exitonclick()
