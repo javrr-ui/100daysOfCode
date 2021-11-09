404d06ea724b7fd32875863e2457565d8b57b92c
Javier <framirez76@uabc.edu.mx>
2021-11-08 22:04:37 -0800

    Draw a square

--- day 18/main.py
+++ day 18/main.py
@@ -1,7 +1,15 @@
 from turtle import Turtle, Screen
 
+
+def draw_square(turtle_obj):
+    for _ in range(4):
+        turtle_obj.forward(100)
+        turtle_obj.right(90)
+
+
 turtle = Turtle()
 turtle.shape("turtle")
 turtle.color("black", "green")
+draw_square(turtle)
 screen = Screen()
 screen.exitonclick()
