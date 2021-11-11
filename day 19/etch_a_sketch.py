7640ebf175856bc58e67eb3834743d3c72a06515
Javier <framirez76@uabc.edu.mx>
2021-11-10 21:19:35 -0800

    Define empty methods

--- day 19/etch_a_sketch.py
+++ day 19/etch_a_sketch.py
@@ -3,5 +3,26 @@ from turtle import Turtle, Screen
 turtle = Turtle()
 screen = Screen()
 
+
+def move_forward():
+    pass
+
+
+def move_backward():
+    pass
+
+
+def rotate_clockwise():
+    pass
+
+
+def rotate_counterclockwise():
+    pass
+
+
+def clear_screen():
+    pass
+
+
 screen.listen()
 screen.exitonclick()
