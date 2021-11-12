93ac41a10db1827855eaf65ea823e7049852365a
Javier <framirez76@uabc.edu.mx>
2021-11-11 21:05:06 -0800

    Create a list filled with colors

--- day 19/turtle_race.py
+++ day 19/turtle_race.py
@@ -3,4 +3,5 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=600, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 screen.exitonclick()
