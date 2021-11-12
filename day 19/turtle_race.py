325b74547a6a59caac28bf877dede7bc67d8726c
Javier <framirez76@uabc.edu.mx>
2021-11-11 21:54:17 -0800

    Create as many turtles as there is in colors list, and set their position at left window border

--- day 19/turtle_race.py
+++ day 19/turtle_race.py
@@ -4,4 +4,14 @@ screen = Screen()
 screen.setup(width=600, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+y_pos = -125
+for color in colors:
+    turtle = Turtle("turtle")
+    turtle.color(color)
+    turtle.penup()
+    turtle.goto(-280, y_pos)
+    y_pos += 50
+
+
 screen.exitonclick()
