adf76c1cc07457bd94890b3c12d80e9b4cfaafda
Javier <framirez76@uabc.edu.mx>
2021-11-11 18:51:37 -0800

    Ask user for text input

--- day 19/turtle_race.py
+++ day 19/turtle_race.py
@@ -2,5 +2,5 @@ from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=600, height=400)
-
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 screen.exitonclick()
