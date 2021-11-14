37c701ae5c65b5f2fcd534de11ad33e312302e24
Javier <framirez76@uabc.edu.mx>
2021-11-14 14:40:36 -0800

    Define method to move snake down

--- day 20/main.py
+++ day 20/main.py
@@ -41,6 +41,11 @@ class Snake:
         self.snake_body[0].setheading(90)
         self.snake_body[0].forward(20)
 
+    def down(self):
+        self.rearrange_body()
+        self.snake_body[0].setheading(270)
+        self.snake_body[0].forward(20)
+
     def rearrange_body(self):
         for segment in range(len(self.snake_body)-1, 0, -1):
             x = self.snake_body[segment - 1].xcor()
