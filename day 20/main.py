268ef6e01f6dc64236dcfde724c3fd67c249560c
Javier <framirez76@uabc.edu.mx>
2021-11-14 11:28:42 -0800

    Define snake function to draw a 3 segment snake on screen

--- day 20/main.py
+++ day 20/main.py
@@ -6,6 +6,14 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 
+def snake():
+    x_coord = 0
+    for i in range(3):
+        snake_segment = Turtle("square")
+        snake_segment.penup()
+        snake_segment.setx(x_coord)
+        snake_segment.color("white")
+        x_coord -= 20
 
 
 screen.exitonclick()
