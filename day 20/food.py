3865853af7e1c1753486fdc282a136f3e31073e0
Javier <framirez76@uabc.edu.mx>
2021-11-18 21:37:44 -0800

    Move code to a method

--- day 20/food.py
+++ day 20/food.py
@@ -10,6 +10,9 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("green")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         x = random.randint(-280, 280)
         y = random.randint(-280, 280)
         self.goto(x, y)
