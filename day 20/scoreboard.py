44821ad86fd0c1c30f65cf91cf9e9827eb1195c8
Javier <framirez76@uabc.edu.mx>
2021-11-19 22:34:00 -0800

    Set color to white, hide turtle, write user score on screen

--- day 20/scoreboard.py
+++ day 20/scoreboard.py
@@ -4,3 +4,6 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         self.scoreboard = Turtle()
+        self.scoreboard.color("white")
+        self.scoreboard.hideturtle()
+        self.scoreboard.write("Score: 0", False, align="center")
