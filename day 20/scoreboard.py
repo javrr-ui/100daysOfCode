aeecbbf87e99b7dabd7f6c1f1963cdae49974fa4
Javier <framirez76@uabc.edu.mx>
2021-11-19 22:34:38 -0800

    Initialize score variable

--- day 20/scoreboard.py
+++ day 20/scoreboard.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
     def __init__(self):
+        self.score = 0
         self.scoreboard = Turtle()
         self.scoreboard.color("white")
         self.scoreboard.hideturtle()
