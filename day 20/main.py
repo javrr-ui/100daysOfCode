f2284b5fe22e8cb6e78d0bcb44d23fcc16add8e5
Javier <framirez76@uabc.edu.mx>
2021-11-21 15:54:07 -0800

    Replace True with a flag

--- day 20/main.py
+++ day 20/main.py
@@ -18,7 +18,8 @@ screen.onkey(snake.right, "d")
 screen.onkey(snake.up, "w")
 screen.onkey(snake.left, "a")
 screen.onkey(snake.down, "s")
-while True:
+game_is_on = True
+while game_is_on:
     screen.update()
     time.sleep(0.10)
     snake.move()
