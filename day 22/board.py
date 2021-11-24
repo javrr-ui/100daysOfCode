797d76105030180660a496576f0e886574f78a3a
Javier <framirez76@uabc.edu.mx>
2021-11-23 18:04:37 -0800

    Make a black screen

--- day 22/board.py
+++ day 22/board.py
@@ -1,1 +1,9 @@
 from turtle import Turtle, Screen
+
+
+class Board:
+    def __init__(self):
+        self.screen = Screen()
+        self.screen.setup(1000, 600)
+        self.screen.bgcolor("black")
+        self.screen.exitonclick()
