50e75b12b4fcdb57dde1941864b9fa68706d6ca4
Javier <framirez76@uabc.edu.mx>
2021-11-23 20:08:55 -0800

    Rename

--- day 22/main.py
+++ day 22/main.py
@@ -3,12 +3,12 @@ from turtle import Turtle, Screen
 
 def dashed_line(screen):
     for i in range(0, screen.window_height(), int(screen.window_height() / 10)):
-        dashed_line = Turtle("square")
-        dashed_line.color("white")
-        dashed_line.shapesize(stretch_len=2, stretch_wid=0.20)
-        dashed_line.penup()
-        dashed_line.tilt(90)
-        dashed_line.goto(0, int(screen.window_height() / 2) - (i * 1.20))
+        line = Turtle("square")
+        line.color("white")
+        line.shapesize(stretch_len=2, stretch_wid=0.20)
+        line.penup()
+        line.tilt(90)
+        line.goto(0, int(screen.window_height() / 2) - (i * 1.20))
 
 
 screen = Screen()
