6f4078580f56484975b531aa3206030cee73444a
Javier <framirez76@uabc.edu.mx>
2021-11-23 20:04:35 -0800

    Make lines thinner and closer form each other

--- day 22/main.py
+++ day 22/main.py
@@ -5,7 +5,7 @@ def dashed_line(screen):
     for i in range(0, screen.window_height(), int(screen.window_height() / 10)):
         dashed_line = Turtle("square")
         dashed_line.color("white")
-        dashed_line.shapesize(stretch_len=3)
+        dashed_line.shapesize(stretch_len=2, stretch_wid=0.20)
         dashed_line.penup()
         dashed_line.tilt(90)
         dashed_line.goto(0, int(self.screen.window_height() / 2) - (i * 1.20))
