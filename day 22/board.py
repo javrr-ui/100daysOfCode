91c33bf8409aeba90f683d43da6ab8e5cff168d1
Javier <framirez76@uabc.edu.mx>
2021-11-23 19:19:28 -0800

    Method to draw a dashed line

--- day 22/board.py
+++ day 22/board.py
@@ -7,3 +7,13 @@ class Board:
         self.screen.setup(1000, 600)
         self.screen.bgcolor("black")
         self.screen.exitonclick()
+
+    def dashed_line(self):
+
+        for i in range(0, self.screen.window_height(), int(self.screen.window_height()/6)):
+            dashed_line = Turtle("square")
+            dashed_line.color("white")
+            dashed_line.shapesize(stretch_len=3)
+            dashed_line.penup()
+            dashed_line.tilt(90)
+            dashed_line.goto(0, int(self.screen.window_height()/2)-(i*1.20))
