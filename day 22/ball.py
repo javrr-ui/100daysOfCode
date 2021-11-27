f097e43baf6c2cc4ab0017140b2f15b8c6378e38
Javier <framirez76@uabc.edu.mx>
2021-11-26 22:09:45 -0800

    Add screen parameter

--- day 22/ball.py
+++ day 22/ball.py
@@ -2,12 +2,13 @@ from turtle import Turtle
 
 
 class Ball(Turtle):
-    def __init__(self):
+    def __init__(self, screen):
         super().__init__()
         self.shape("circle")
         self.color("white")
         self.penup()
         self.goto(0, 0)
+        self.screen = screen
 
     def move(self):
         self.goto(self.xcor()+10, self.ycor()+10)
