754e4e3717d54a6fa9ab48c3fd5630559940cb91
Javier <framirez76@uabc.edu.mx>
2021-11-26 23:28:05 -0800

    Set wall bounds and flags

--- day 22/ball.py
+++ day 22/ball.py
@@ -10,5 +10,13 @@ class Ball(Turtle):
         self.goto(0, 0)
         self.screen = screen
 
+        self.upper_limit = self.screen.window_height() / 2 - 20  # -20 pixel gap
+        self.lower_limit = (self.screen.window_height() / 2 * -1) + 20  # +20 pixel gap
+        self.left_limit = self.screen.window_width() / 2 * -1 + 10  # +10 pixel gap
+        self.right_limit = self.screen.window_width() / 2 - 30  # 30 pixel gap
+
+    positive_x = True
+    positive_y = True
+
     def move(self):
         self.goto(self.xcor()+10, self.ycor()+10)
