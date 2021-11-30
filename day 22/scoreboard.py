b7380bab6f02775e27d280e83ef3ae1724576654
Javier <framirez76@uabc.edu.mx>
2021-11-29 19:34:53 -0800

    Define method to update scoreboard

--- day 22/scoreboard.py
+++ day 22/scoreboard.py
@@ -19,3 +19,11 @@ class Scoreboard(Turtle):
 
     def increase_r_score(self):
         self.r_score += 1
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
