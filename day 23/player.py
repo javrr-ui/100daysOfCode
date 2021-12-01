b6bd94e483c32c9d46d0b8e80539cc0ee52ba139
Javier <framirez76@uabc.edu.mx>
2021-11-30 21:32:32 -0800

    Lift pen before moving turtle

--- day 23/player.py
+++ day 23/player.py
@@ -5,6 +5,6 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
+        self.penup()
         self.setheading(90)
         self.goto(0, -280)
-        self.penup()
