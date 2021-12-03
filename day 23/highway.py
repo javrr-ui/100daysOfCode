284bd8689d54addfcc7100fe6867c8463e564a2a
Javier <framirez76@uabc.edu.mx>
2021-12-02 20:40:47 -0800

    Code reformat

--- day 23/highway.py
+++ day 23/highway.py
@@ -5,7 +5,7 @@ from random import randrange
 
 class Highway:
     def __init__(self, screen):
-        self.highway_height = (screen.window_height()/2) - 50
+        self.highway_height = (screen.window_height() / 2) - 50
         self.highway_width = screen.window_width() / 2
         self.highway_limits()
 
