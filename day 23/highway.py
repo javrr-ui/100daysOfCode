be558267a9f57104b97f9f058f7b5583ddb6e97a
Javier <framirez76@uabc.edu.mx>
2021-12-02 19:24:31 -0800

    Define method to draw highway limits

--- day 23/highway.py
+++ day 23/highway.py
@@ -6,3 +6,14 @@ class Highway:
     def __init__(self, screen):
         self.highway_height = (screen.window_height()/2) - 50
         self.highway_width = screen.window_width() / 2
+
+    def highway_limits(self):
+        line = Turtle()
+        line.penup()
+        line.goto(self.highway_width, self.highway_height)
+        line.pendown()
+        line.goto(-self.highway_width, self.highway_height)
+        line.penup()
+        line.goto(self.highway_width, -self.highway_height)
+        line.pendown()
+        line.goto(-self.highway_width, -self.highway_height)
