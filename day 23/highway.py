05c741b3c0da712f6a87a963b953f0395a9db410
Javier <framirez76@uabc.edu.mx>
2021-12-02 19:25:04 -0800

    Call highway limits method

--- day 23/highway.py
+++ day 23/highway.py
@@ -6,6 +6,7 @@ class Highway:
     def __init__(self, screen):
         self.highway_height = (screen.window_height()/2) - 50
         self.highway_width = screen.window_width() / 2
+        self.highway_limits()
 
     def highway_limits(self):
         line = Turtle()
