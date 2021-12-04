3e7590dec1aad3873e110158b7098f0fed54ae79
Javier <framirez76@uabc.edu.mx>
2021-12-04 14:02:35 -0800

    Set scoreboard position

--- day 23/scoreboard.py
+++ day 23/scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
         self.level = 1
         self.color("black")
         self.hideturtle()
+        self.goto(-250, 260)
         self.update()
 
     def update(self):
