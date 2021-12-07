12ab6a3b047fa43bfa519dd8098588c0f6341721
Javier <framirez76@uabc.edu.mx>
2021-12-06 23:17:41 -0800

    Declare high_score variable

--- day 20/scoreboard.py
+++ day 20/scoreboard.py
@@ -5,6 +5,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.goto(0, 260)
