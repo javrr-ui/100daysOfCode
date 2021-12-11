c2de1fb49910b15cd8b691b728082fedca1461c3
Javier <framirez76@uabc.edu.mx>
2021-12-11 15:28:56 -0800

    Comment out code

--- day 25/main.py
+++ day 25/main.py
@@ -1,11 +1,11 @@
 # with open("weather_data.csv") as file:
 #     data = list(map(str.strip, file.readlines()))
 
-import csv
-
-with open("weather_data.csv") as weather_file:
-    data = csv.reader(weather_file)
-    temperatures = [row[1] for row in data]
-    temperatures.__delitem__(0)
-    temperatures = [int(temp) for temp in temperatures]
-    print(temperatures)
+# import csv
+#
+# with open("weather_data.csv") as weather_file:
+#     data = csv.reader(weather_file)
+#     temperatures = [row[1] for row in data]
+#     temperatures.__delitem__(0)
+#     temperatures = [int(temp) for temp in temperatures]
+#     print(temperatures)
