0b032e9878b9a94f332aa8c95f7b3e2617b8303d
Javier <framirez76@uabc.edu.mx>
2021-12-10 21:30:04 -0800

    Read starting letter

--- day 24/mail-merge-project/main.py
+++ day 24/mail-merge-project/main.py
@@ -6,3 +6,6 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("input/letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
