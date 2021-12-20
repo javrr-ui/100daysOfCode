90a83364923b84767ea9e4a10f98f44339d92996
Javier <framirez76@uabc.edu.mx>
2021-12-19 17:25:34 -0800

    Create turtle object

--- day 25/us-states-game/main.py
+++ day 25/us-states-game/main.py
@@ -6,6 +6,7 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+text = turtle.Turtle()
 
 answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 answer = answer.title()
