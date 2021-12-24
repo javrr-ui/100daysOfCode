629cd363caeadea57c441b96cb895454ce2586a7
Javier <framirez76@uabc.edu.mx>
2021-12-23 20:04:05 -0800

    Solution

---
+++ day 26/main.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4]
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+
+# new_list = [num * 2 for num in range(1, 5)]
+# print(new_list)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name.upper() for name in names if len(name) > 5]
+print(short_names)
