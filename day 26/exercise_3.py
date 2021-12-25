420188677b4cd0e4f475dd12d54a9e6a71dc091c
Javier <framirez76@uabc.edu.mx>
2021-12-24 19:45:27 -0800

    Open file2 and store data into a list

--- day 26/exercise_3.py
+++ day 26/exercise_3.py
@@ -3,6 +3,10 @@ with open("file1.txt") as file1:
     list_1 = file1.readlines()
     list_1 = [int(number.strip()) for number in list_1]
 
+with open("file2.txt") as file2:
+    list_1 = file2.readlines()
+    list_1 = [int(number.strip()) for number in list_2]
+
 # Write your code above 👆
 
 print(result)
