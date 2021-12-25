28fbd05db4938212ffb5bda3bdd3fc8f799dba67
Javier <framirez76@uabc.edu.mx>
2021-12-24 19:59:32 -0800

    Store numbers that are common in both lists

--- day 26/exercise_3.py
+++ day 26/exercise_3.py
@@ -7,6 +7,8 @@ with open("file2.txt") as file2:
     list_2 = file2.readlines()
     list_2 = [int(number.strip()) for number in list_2]
 
+result = [number for number in list_1 if number in list_2]
+
 # Write your code above 👆
 
 print(result)
