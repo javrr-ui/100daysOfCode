c9b1c0b6a3d0fc92f07fcb6ae6dc41d55fcc445d
Javier <framirez76@uabc.edu.mx>
2021-12-26 13:55:59 -0800

    Read csv file with nato alphabet, create a dictionary out of alphabet data frame

--- day 26/NATO-alphabet/main.py
+++ day 26/NATO-alphabet/main.py
@@ -23,5 +23,9 @@ for (index, row) in student_data_frame.iterrows():
 
 # TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
+alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+alpha_dict = {row.letter: row.code for (index, row) in alphabet.iterrows()}
+
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
