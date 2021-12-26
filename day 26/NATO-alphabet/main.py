0282eb2a29b3a32d00622916d05a88709b57d9ef
Javier <framirez76@uabc.edu.mx>
2021-12-26 14:00:28 -0800

    Upper case word

--- day 26/NATO-alphabet/main.py
+++ day 26/NATO-alphabet/main.py
@@ -30,4 +30,5 @@ alpha_dict = {row.letter: row.code for (index, row) in alphabet.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ")
+word = input("Enter a word: ").upper()
+
