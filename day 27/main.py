6f07d38eb2d862e0e1d1ab9c61e0b4755d7080c9
Javier <framirez76@uabc.edu.mx>
2021-12-27 20:43:43 -0800

    Add label to window

--- day 27/main.py
+++ day 27/main.py
@@ -3,4 +3,7 @@ import tkinter
 window = tkinter.Tk()
 window.title("GUI Using Tkinter")
 window.minsize(width=500, height=300)
+
+label = tkinter.Label(text="A label")
+label.pack()
 window.mainloop()
