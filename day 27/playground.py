d2ceae5451f352df263db23c1f4de252f09d89d7
Javier <framirez76@uabc.edu.mx>
2021-12-28 22:07:14 -0800

    **kwargs example

--- day 27/playground.py
+++ day 27/playground.py
@@ -3,3 +3,14 @@ def add(*args):
 
 
 print(add(3, 5))
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    print(n)
+
+
+print(calculate(3, add=5, multiply=4))
