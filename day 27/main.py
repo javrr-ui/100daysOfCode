8ab8c3d0701b7a4d4e7bc9fc310d9ae538ce3ad9
Javier <framirez76@uabc.edu.mx>
2021-12-30 23:39:28 -0800

    Use grid instead of pack

--- day 27/main.py
+++ day 27/main.py
@@ -5,7 +5,7 @@ window.title("GUI Using Tkinter")
 window.minsize(width=500, height=300)
 
 label = tkinter.Label(text="A label")
-label.pack()
+label.grid(column=0, row=0)
 
 
 # Button
@@ -14,11 +14,11 @@ def clicked():
 
 
 button = tkinter.Button(text="Click me", command=clicked)
-button.pack()
+button.grid(column=1, row=0)
 
 #Entry
 
 input = tkinter.Entry(width=20)
-input.pack()
+input.grid(column=2, row=0)
 
 window.mainloop()
