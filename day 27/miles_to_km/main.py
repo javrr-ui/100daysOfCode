f2a577b5a6e300b7f58af18b542887e5d34ee40f
Javier <framirez76@uabc.edu.mx>
2021-12-31 22:15:05 -0800

    Add entry for miles

--- day 27/miles_to_km/main.py
+++ day 27/miles_to_km/main.py
@@ -6,4 +6,7 @@ window.title("Mile to Km converter")
 label1 = Label(text="is equal to")
 label1.grid(column=0, row=1)
 
+miles_entry = Entry()
+miles_entry.grid(column=1, row=0)
+
 window.mainloop()
