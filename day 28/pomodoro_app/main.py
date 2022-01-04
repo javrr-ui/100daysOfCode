d6bff4f772ea0b14367d0172b916e5b41ad3a6ec
Javier <framirez76@uabc.edu.mx>
2022-01-03 22:34:58 -0800

    Add label with the text "Timer"

--- day 28/pomodoro_app/main.py
+++ day 28/pomodoro_app/main.py
@@ -19,6 +19,10 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro App")
 window.config(padx=100, pady=50, bg=YELLOW)
+
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=("Times New Roman", 30, "bold"))
+timer_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
