61fa9d8fc4979374a3baf207c8a20ac032587bb2
Javier <framirez76@uabc.edu.mx>
2022-01-06 23:18:57 -0800

    start timer  when count is not greater than zero

--- day 28/pomodoro_app/main.py
+++ day 28/pomodoro_app/main.py
@@ -43,6 +43,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
