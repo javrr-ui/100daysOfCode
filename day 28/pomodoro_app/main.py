f4e3e92306377b0c71ed031788a98b63e1ca9efc
Javier <framirez76@uabc.edu.mx>
2022-01-07 13:02:20 -0800

    Change title text when it is long break, short break or work time

--- day 28/pomodoro_app/main.py
+++ day 28/pomodoro_app/main.py
@@ -24,10 +24,13 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
