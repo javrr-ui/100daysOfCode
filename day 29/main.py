cb941cbb3c46b40e2b6fabf8a0399c65b4de8cad
Javier <framirez76@uabc.edu.mx>
2022-01-09 13:32:30 -0800

    Add 20px padding

--- day 29/main.py
+++ day 29/main.py
@@ -7,6 +7,7 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
+window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
