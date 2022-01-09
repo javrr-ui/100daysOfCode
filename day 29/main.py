7310f1006b0c211a8f36d6bb553674038becca4c
Javier <framirez76@uabc.edu.mx>
2022-01-09 14:13:14 -0800

    Switch from pack to grid layout

--- day 29/main.py
+++ day 29/main.py
@@ -12,6 +12,6 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
-canvas.pack()
+canvas.grid(column=1, row=0)
 
 window.mainloop()
