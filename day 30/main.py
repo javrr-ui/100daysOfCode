2443160414049ddc655d75b44ae9eeffe9de73a5
Javier <framirez76@uabc.edu.mx>
2022-01-11 23:26:20 -0800

    Read file when there's no exceptions

--- day 30/main.py
+++ day 30/main.py
@@ -6,3 +6,6 @@ except FileNotFoundError:
     file = open("a_file.txt", "w")
 except KeyError as error_message:
     print(f"The key {error_message} does not exist")
+else:
+    content = file.read()
+    print(content)
