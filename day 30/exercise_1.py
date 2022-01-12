6cfdae3f2f19416b09f8647f3614c6c3415a88f9
Javier <framirez76@uabc.edu.mx>
2022-01-12 13:32:19 -0800

    Complete exercise

--- day 30/exercise_1.py
+++ day 30/exercise_1.py
@@ -1,9 +1,13 @@
 fruits = ["Apple", "Pear", "Orange"]
 
-#TODO: Catch the exception and make sure the code runs without crashing.
+
+# TODO: Catch the exception and make sure the code runs without crashing.
 def make_pie(index):
-    fruit = fruits[index]
-    print(fruit + " pie")
+    try:
+        fruit = fruits[index]
+        print(fruit + " pie")
+    except IndexError:
+        print("Fruit pie")
 
 
 make_pie(4)
