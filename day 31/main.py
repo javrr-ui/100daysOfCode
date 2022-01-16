86a87a68ffe01c28b1ccb3f47861a7c7fd65656e
Javier <framirez76@uabc.edu.mx>
2022-01-15 23:57:22 -0800

    Add button for wrong answers

--- day 31/main.py
+++ day 31/main.py
@@ -9,4 +9,9 @@ card_back = PhotoImage(file="images/card_back.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_back)
 canvas.grid(column=0, row=0, columnspan=2)
+
+wrong = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong, bg=BACKGROUND_COLOR)
+wrong_button.grid(column=0, row=1)
+
 window.mainloop()
