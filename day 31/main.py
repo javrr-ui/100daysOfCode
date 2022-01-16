8fc801f56607624b931849e45644144326a069bb
Javier <framirez76@uabc.edu.mx>
2022-01-15 23:45:31 -0800

    Create canvas, add image

--- day 31/main.py
+++ day 31/main.py
@@ -5,4 +5,8 @@ window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50)
 
+card_back = PhotoImage(file="images/card_back.png")
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR)
+canvas.create_image(400, 263, image=card_back)
+canvas.grid(column=0, row=0, columnspan=2)
 window.mainloop()
