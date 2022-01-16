0762218d54d0f0b0243daec3a5b141cfe99bf72e
Javier <framirez76@uabc.edu.mx>
2022-01-15 23:52:08 -0800

    Set highlightthickness to zero

--- day 31/main.py
+++ day 31/main.py
@@ -6,7 +6,7 @@ window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 card_back = PhotoImage(file="images/card_back.png")
-canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_back)
 canvas.grid(column=0, row=0, columnspan=2)
 window.mainloop()
