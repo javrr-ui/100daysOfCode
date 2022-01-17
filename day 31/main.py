26c9467cc995bc31ea4f4a21836e8ffdd6238b4f
Javier <framirez76@uabc.edu.mx>
2022-01-16 23:34:45 -0800

    Add card word

--- day 31/main.py
+++ day 31/main.py
@@ -9,6 +9,7 @@ card_back = PhotoImage(file="images/card_back.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_back)
 canvas.create_text(400, 150, text="Title", font="Arial 40 italic")
+canvas.create_text(400, 263, text="Word", font="Arial 60 bold")
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong = PhotoImage(file="images/wrong.png")
