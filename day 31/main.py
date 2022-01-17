e5a8b649d5af3e17fc44d00ffae63cf973fbe937
Javier <framirez76@uabc.edu.mx>
2022-01-16 23:56:05 -0800

    Read csv file

--- day 31/main.py
+++ day 31/main.py
@@ -5,6 +5,8 @@ window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+words = pandas.read_csv("data/french_words.csv")
+
 card_front = PhotoImage(file="images/card_front.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front)
