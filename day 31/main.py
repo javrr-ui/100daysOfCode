f8cb8c819666ff57715c1737ebf7aa0fd6be1c69
Javier <framirez76@uabc.edu.mx>
2022-01-20 21:49:20 -0800

    Try to read data from a file called words_to_learn.csv, if file is not found fetch data from french_words.csv and create file mentioned before

--- day 31/main.py
+++ day 31/main.py
@@ -7,7 +7,12 @@ window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-words = pandas.read_csv("data/french_words.csv")
+try:
+    words = pandas.read_csv("data/words_to_learn.csv")
+except:
+    words = pandas.read_csv("data/french_words.csv")
+    words.to_csv("data/words_to_learn.csv", index=False)
+
 word_count = len(words)
 words = words.to_dict(orient="records")
 shuffle(words)
