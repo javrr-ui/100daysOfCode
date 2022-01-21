48eb41fc0e66204228be105ea4da4f8063356218
Javier <framirez76@uabc.edu.mx>
2022-01-20 20:56:35 -0800

    Move imports at the top of the file

--- day 31/main.py
+++ day 31/main.py
@@ -1,7 +1,8 @@
-BACKGROUND_COLOR = "#B1DDC6"
 from tkinter import *
 from pandas import *
 from random import choice, shuffle
+
+BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
