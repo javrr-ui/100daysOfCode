b413d1b598800e879391b6b035a81f285a55aced
Javier <framirez76@uabc.edu.mx>
2022-01-22 23:53:12 -0800

    Send email to dummy account

--- day 32/main.py
+++ day 32/main.py
@@ -1,8 +1,9 @@
 import smtplib
 
 email = "javiercrack351@gmail.com"
-password = "asdfsadfs"
+password = "asdadada3v423v"
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=email, password=password)
+connection.sendmail(from_addr=email, to_addrs="hello@gmail.com", msg="Hello XD")
