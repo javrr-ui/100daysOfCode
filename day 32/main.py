2d749bf90877b69ab35bf19d44c297d7816d442d
Javier <framirez76@uabc.edu.mx>
2022-01-23 23:46:59 -0800

    Refactor code

--- day 32/main.py
+++ day 32/main.py
@@ -3,7 +3,7 @@ import smtplib
 email = "javiercrack351@gmail.com"
 password = "asdadada3v423v"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=email, password=password)
-connection.sendmail(from_addr=email, to_addrs="hello@gmail.com", msg="Subject:Hello\n\nHello XD")
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=email, password=password)
+    connection.sendmail(from_addr=email, to_addrs="hello@gmail.com", msg="Subject:Hello\n\nHello XD")
