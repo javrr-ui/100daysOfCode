716bedea9a9c93862c64ea788e7d4e51aea68341
Javier <framirez76@uabc.edu.mx>
2022-01-26 23:24:51 -0800

    Get actual date

--- day 32/birthdayWisher/main.py
+++ day 32/birthdayWisher/main.py
@@ -8,6 +8,7 @@ birthdays = data.to_dict(orient="records")
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
+date = datetime.datetime.now()
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
