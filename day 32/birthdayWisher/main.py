7f1c9ae206b5960543730d325084f7edf0ec6a8a
Javier <framirez76@uabc.edu.mx>
2022-01-26 23:48:36 -0800

    Check if list is not empty, iterate list if it's not empty, generate a random number between 1 and 3

--- day 32/birthdayWisher/main.py
+++ day 32/birthdayWisher/main.py
@@ -17,6 +17,10 @@ for person in birthdays:
         congratulations_list.append(person)
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+if len(congratulations_list) > 0:
+    for person in congratulations_list:
+        number = randint(1, 3)
+
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
