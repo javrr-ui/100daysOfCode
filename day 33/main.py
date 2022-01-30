1e793e690719e5d2d102d32c884ca01fdc035161
Javier <framirez76@uabc.edu.mx>
2022-01-29 20:18:46 -0800

    Get hour

--- day 33/main.py
+++ day 33/main.py
@@ -22,8 +22,8 @@ response.raise_for_status()
 
 data = response.json()
 
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
 time = datetime.datetime.now()
 
