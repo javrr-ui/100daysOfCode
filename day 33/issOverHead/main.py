acb071ea0423e24a190c5a964633c4262de300ad
Javier <framirez76@uabc.edu.mx>
2022-01-30 12:48:18 -0800

    Code reformat

--- day 33/issOverHead/main.py
+++ day 33/issOverHead/main.py
@@ -9,8 +9,8 @@ with open("credentials.json", "r") as file:
     email = credentials["email"]
     password = credentials["password"]
 
-MY_LAT = 19.377713 # Your latitude
-MY_LONG = -99.105526 # Your longitude
+MY_LAT = 19.377713  # Your latitude
+MY_LONG = -99.105526  # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -19,7 +19,7 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
+# Your position is within +5 or -5 degrees of the ISS position.
 
 
 parameters = {
@@ -36,7 +36,7 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
-#If the ISS is close to my current position
+# If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
