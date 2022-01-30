c810622333f0a1ff7da3fdab6a6b945bf17c5357
Javier <framirez76@uabc.edu.mx>
2022-01-29 18:46:49 -0800

    Rename variable

--- day 33/kanyeQuotesApp/main.py
+++ day 33/kanyeQuotesApp/main.py
@@ -4,7 +4,8 @@ import requests
 def get_quote():
     response = requests.get(url="https://api.kanye.rest/")
     response.raise_for_status()
-    date = response.json()
+    data = response.json()
+
 
 
 
