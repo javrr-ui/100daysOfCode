dbf9ef99cd38f73b4f292f43d2a8460d16365e4f
Javier <framirez76@uabc.edu.mx>
2022-02-01 22:44:48 -0800

    Add score label

--- day 34/ui.py
+++ day 34/ui.py
@@ -8,4 +8,7 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR)
+        self.score_label = Label(text="Score: 0")
+        self.score_label.grid(column=1, row=0)
+
         self.window.mainloop()
