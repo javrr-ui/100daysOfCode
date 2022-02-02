590ba7096c50202cd17eafaa3555ab691dbaf5f1
Javier <framirez76@uabc.edu.mx>
2022-02-01 22:48:19 -0800

    Add 20px padding

--- day 34/ui.py
+++ day 34/ui.py
@@ -7,7 +7,7 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(bg=THEME_COLOR)
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
         self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
         self.score_label.grid(column=1, row=0)
 
