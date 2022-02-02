66845359efef7dc1a6b8fa643716544ebc82536a
Javier <framirez76@uabc.edu.mx>
2022-02-01 23:23:46 -0800

    Add canvas with text

--- day 34/ui.py
+++ day 34/ui.py
@@ -11,4 +11,8 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
         self.score_label.grid(column=1, row=0)
 
+        self.question_canvas = Canvas(width=300, height=250)
+        self.question_canvas.create_text(150, 125, width=300, text="Text", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.question_canvas.grid(column=0, row=1, columnspan=2)
+
         self.window.mainloop()
