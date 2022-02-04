d3ee389118024851893ed2616b8eacdcf89f387a
Javier <framirez76@uabc.edu.mx>
2022-02-03 22:29:37 -0800

    Add parameter

--- day 34/ui.py
+++ day 34/ui.py
@@ -4,7 +4,7 @@ THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
