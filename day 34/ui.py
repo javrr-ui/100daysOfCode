b9ca4038be327310db213bf42144d7147b2d062a
Javier <framirez76@uabc.edu.mx>
2022-02-03 22:35:08 -0800

    declare quiz

--- day 34/ui.py
+++ day 34/ui.py
@@ -5,6 +5,7 @@ THEME_COLOR = "#375362"
 
 class QuizInterface:
     def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
