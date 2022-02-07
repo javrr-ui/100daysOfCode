aa6bc70f5edefcf8ec3f3d5aa33e5df93954037f
Javier <framirez76@uabc.edu.mx>
2022-02-06 23:02:49 -0800

    api call

--- day 35/rain_alert/main.py
+++ day 35/rain_alert/main.py
@@ -1,3 +1,6 @@
 import requests
 
 api_key = "6305e293bf13b8ae394e4574bf17e79d"
+url = f"https://api.openweathermap.org/data/2.5/onecall?lat={19.432608}&lon={-99.133209}&appid={api_key}"
+response = requests.get(url)
+
