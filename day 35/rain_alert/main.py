2165a535c15aba15bbb8921295693b92e28809e6
Javier <framirez76@uabc.edu.mx>
2022-02-06 23:04:04 -0800

    Print status code

--- day 35/rain_alert/main.py
+++ day 35/rain_alert/main.py
@@ -3,4 +3,4 @@ import requests
 api_key = "6305e293bf13b8ae394e4574bf17e79d"
 url = f"https://api.openweathermap.org/data/2.5/onecall?lat={19.432608}&lon={-99.133209}&appid={api_key}"
 response = requests.get(url)
-
+print(f"Status code: {response.status_code}")
